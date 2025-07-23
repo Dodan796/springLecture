e8ea8d34e090ebf6334c15d771413aefaa025a02
je.lee <dodan0825@gmail.com>
2025-07-23 22:32:18 +0900

    주석추가

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -9,8 +9,14 @@ public class HelloController {
 
     @GetMapping("hello")
     public String hello(Model model){
+        // model 안에 data 변수를 만들어서 hello!! 입력값을 넣어놓음
         model.addAttribute("data","hello!!");
+
+        // templates 패키지 아래 hello 파일로 이동한다.
+        // Springboot 템플릿엔진의 default 경로는
+        // => resources:templates/ + {ViewName}+ .html로 바로 연동이 된다.
         return "hello";
+
     }
 
 }
