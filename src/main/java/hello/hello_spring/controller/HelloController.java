46e8ee4387851fd2ea2d4996abf72a1258fd1fdf
je.lee <dodan0825@gmail.com>
2025-07-23 23:05:10 +0900

    Spring_MVC

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -3,20 +3,25 @@ package hello.hello_spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
     @GetMapping("hello")
-    public String hello(Model model){
+    public String hello(Model model) {
         // model 안에 data 변수를 만들어서 hello!! 입력값을 넣어놓음
-        model.addAttribute("data","hello!!");
+        model.addAttribute("data", "hello!!");
 
         // templates 패키지 아래 hello 파일로 이동한다.
         // Springboot 템플릿엔진의 default 경로는
         // => resources:templates/ + {ViewName}+ .html로 바로 연동이 된다.
         return "hello";
-
     }
 
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello-template";
+    }
 }
