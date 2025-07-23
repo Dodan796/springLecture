f7249f11d1dd5ea89e0be094f9b4f4300e0a55ff
je.lee <dodan0825@gmail.com>
2025-07-23 23:20:56 +0900

    Spring_API

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -20,8 +21,42 @@ public class HelloController {
     }
 
     @GetMapping("hello-mvc")
-    public String helloMvc(@RequestParam("name") String name, Model model){
-        model.addAttribute("name",name);
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
         return "hello-template";
     }
+
+    // @ResponseBody => HTTP에서 Body 부분에 return값을 바로 넣는 어노테이션
+    @ResponseBody
+    @GetMapping("hello-string")
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+
+        // Property 접근방식
+        // Getter: 객체의 private 필드 값을 외부에서 읽을 수 있도록 해주는 메서드
+        public String getName() {
+            return name;
+        }
+
+        // Setter: 객체의 private 필드 값을 외부에서 변경할 수 있도록 해주는 메서드
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
 }
+
+
